1660545704dfaa479f2264c5589bd8bb339671eb
Domantas <domantas.savickas@gmail.com>
2025-12-03 09:56:49 +0200

    Draw circle on screen

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,38 @@
+#include <Adafruit_GFX.h>     // Core graphics library
+#include <Adafruit_ST7789.h>  // Hardware-specific library for ST7789
 #include <Arduino.h>
+#include <SPI.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// Screen parameters
+constexpr uint16_t TFT_WIDTH = 240;
+constexpr uint16_t TFT_HEIGHT = 280;
+
+constexpr int8_t TFT_CS = WB_SPI_CS;
+constexpr int8_t TFT_RST = 27;
+constexpr int8_t TFT_DC = WB_SW1;
+
+Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
+
+void drawCompass() {
+    constexpr int16_t radius = 100;
+    constexpr uint16_t background_color = ST77XX_BLACK;
+    constexpr uint16_t color = ST77XX_GREEN;
+
+    tft.fillScreen(background_color);
+    tft.drawCircle(TFT_WIDTH / 2, TFT_HEIGHT / 2, radius, color);
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    Serial.begin(9600);
+    Serial.print(F("Serial initialized"));
+
+    tft.init(TFT_WIDTH, TFT_HEIGHT);
+    Serial.println(F("Screen initialized"));
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    uint32_t time = millis();
+    drawCompass();
+    Serial.printf("Drew compass at %d\n", time);
+    delay(500);
 }
